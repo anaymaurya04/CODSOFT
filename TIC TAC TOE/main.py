766c5783eaadc6f4bb92e28c0eb07b1fed443f22
Anay Maurya <anaymaurya04@gmail.com>
2024-06-28 13:03:50 +0530

    Tic Tac Toe DOne

--- TIC TAC TOE/main.py
+++ TIC TAC TOE/main.py
@@ -20,7 +20,7 @@ circle_rad= square//3
 circle_width=15
 cross_width=25
 
-screen=pygame.display.set_mode(width,height)
+screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("TIC TAC TOE AI")
 screen.fill(black)
 
@@ -50,8 +50,8 @@ def isboardfull(check_board=board):
     for row in range(board_row):
         for col in range (board_col):
             if check_board[row][col] == 0:
-                return True
-    return False
+                return False
+    return True
 
 def check_win(player, check_board):
     for col in range(board_col):
@@ -65,3 +65,112 @@ def check_win(player, check_board):
     if check_board[0][2]== player and check_board[1][1]== player and check_board[2][0]== player:
         return True
     return False
+
+def minimax(minimax_board, depth, is_maximing):
+    if check_win(2,minimax_board):
+        return float('inf')
+    elif check_win(1,minimax_board):
+        return float('-inf')
+    elif isboardfull(minimax_board):
+        return 0
+    
+    if is_maximing:
+        best_score=-1000
+        for row in range(board_row):
+            for col in range(board_col):
+                if minimax_board[row][col]==0:
+                    minimax_board[row][col] = 2
+                    score = minimax(minimax_board, depth+1, False)
+                    minimax_board[row][col] = 0
+                    best_score= max(score,best_score)
+        return best_score
+    else:
+        best_score=1000
+        for row in range(board_row):
+            for col in range(board_col):
+                if minimax_board[row][col]==0:
+                    minimax_board[row][col] = 1
+                    score = minimax(minimax_board, depth+1, True)
+                    minimax_board[row][col] = 0
+                    best_score= min(score,best_score)
+        return best_score
+    
+def best_move():
+    best_score = -1000
+    move=(-1,-1)
+    for row in range(board_row):
+        for col in range(board_col):
+            if board[row][col]==0:
+                board[row][col]=2
+                score = minimax(board, 0, False)
+                board[row][col]=0
+                if score> best_score:
+                    best_score = score
+                    move = (row,col)
+
+    if move != (-1,-1):
+        mark_sq(move[0], move[1], 2)
+        return True
+    return False
+
+def restart():
+    screen.fill(black)
+    draw_lines()
+    for row in range(board_row):
+        for col in range(board_col):
+            board[row][col]=0
+
+draw_lines()
+
+player =1
+game_over = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+            mouseX = event.pos[0]
+            mouseY = event.pos[1]
+
+            row = mouseY // square
+            col = mouseX // square
+
+            if available_sq(row, col):
+                mark_sq(row, col, player)
+                if check_win(player, board):
+                    game_over = True
+                player = player % 2 + 1
+
+            if not game_over:
+                if best_move():
+                    if check_win(2, board):
+                        game_over = True
+                    player = player % 2 + 1
+
+            if not game_over:
+                if isboardfull():
+                    game_over = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                restart()
+                game_over = False
+                player = 1
+
+    if not game_over:
+        draw_figures()
+    else:
+        if check_win(1, board):
+            draw_figures(green)
+            draw_lines(green)
+        elif check_win(2, board):
+            draw_figures(red)
+            draw_lines(red)
+        else:
+            draw_figures(grey)
+            draw_lines(grey)
+
+    pygame.display.update()
+
+
